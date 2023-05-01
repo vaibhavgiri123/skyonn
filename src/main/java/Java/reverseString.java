ad507f316fc798aae64cb74629a50a8db2dd36b5
vaibh <vaibhavgosavi0206@gmail.com>
2023-05-01 15:17:43 +0530

    ccccccc

---
+++ src/main/java/Java/reverseString.java
@@ -0,0 +1,18 @@
+package Java;
+
+public class reverseString {
+
+	public static void main(String[] args) {
+		String a="ramji";
+		int count=0;
+		for(int i=a.length()-1;i>=0;i--)
+		{
+			System.out.print(a.charAt(i));
+			count++;
+	
+		}
+		System.out.println(count);
+     System.out.println("ram");
+	}
+
+}
