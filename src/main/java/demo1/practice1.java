eba726e0211021288c7f6078096db0bd12b868a1
Vaibhav Gosavi <vaibhavdgosavi0206@gmail.com>
2023-05-05 10:49:45 +0530

    commited

---
+++ src/main/java/demo1/practice1.java
@@ -0,0 +1,18 @@
+package demo1;
+
+
+
+public class practice1 {
+public static void main(String[] args) {
+	
+	
+	
+	
+RestAssured.baseURI=" http://localhost:3000/posts";
+RequestSpecification http=RestAssured.given();
+	
+	
+	
+	
+}
+}
---
+++ src/main/java/demo2/tutorial1.java
@@ -0,0 +1,9 @@
+package demo2;
+
+public class tutorial1 {
+
+	public static void main(String[] args) {
+	System.out.println("test case passes");
+	}
+
+}
